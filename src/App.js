ef36f8ba9613c35449531b9205fcd4eab48b59c5
Alfred1352 <penarandaalfred@outlook.com>
2023-05-03 16:14:14 -0700

    Added row scroll feature and add favorites feature

--- src/App.js
+++ src/App.js
@@ -1,22 +1,58 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import Search from './components/Search';
+import AddFavorites from './components/AddFavorites';
 
 function App() {
+
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+  const [favorites, setFavorites] = useState([]);
+
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=1cdea750`
+
+    const response = await fetch(url);
+    const responseJSON = await response.json();
+
+    if (responseJSON.Search) {
+      setMovies(responseJSON.Search);
+    }
+
+  }
+
+  useEffect(() => {
+    getMovieRequest(searchValue);
+  }, [searchValue])
+
+  const addFavoriteMovie = (movie) => {
+    const newFavoriteList = [...favorites, movie];
+    setFavorites(newFavoriteList);
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid movie-app">
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+        <MovieListHeading heading='Movies'/>
+        <Search searchValue={searchValue} setSearchValue={setSearchValue}/>
+      </div>
+      <div className="row"> 
+        <MovieList movies={movies} handleFavoritesClick={addFavoriteMovie} favoriteComponent={AddFavorites}/>
+      </div>
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+        <MovieListHeading heading='Favorite Movie'/>
+      </div>
+      <div className='row'> 
+        <MovieList movies={favorites} handleFavoritesClick={addFavoriteMovie} favoriteComponent={AddFavorites}/>
+      </div>
+
     </div>
   );
 }
